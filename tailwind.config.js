79621464f6b36b218c3f730c55221547911b33ac
Kenjiro Kosaka <inoshirou@gmail.com>
2025-10-31 08:47:53 +0900

    Fix Tailwind melon theme build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{vue,js,ts}',
     './components/**/*.{vue,js,ts}',
     './layouts/**/*.{vue,js,ts}',
     './pages/**/*.{vue,js,ts}',
